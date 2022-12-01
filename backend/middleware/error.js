81c095c68da0579166f3990f3f8b0acf6cda3dfa
naveen8801 <naveensharma10d@gmail.com>
2022-12-01 15:51:36 +0530

    Fix: Error msg backend

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,19 +4,16 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
 
-  // Mongoose bad object id
   if (err.name === 'CastError') {
     const message = `Resource not found `;
     error = new ErrorResponse(message, 404);
   }
 
-  // Mongoose Duplicate key error
   if (err.code === 11000) {
     const message = 'Duplicate field value entered';
     error = new ErrorResponse(message, 400);
   }
 
-  // Mongoose Validation errors
   if (err.name === 'ValidationError') {
     const message = Object.values(err.errors).map((val) => val.message);
     error = new ErrorResponse(message, 400);
